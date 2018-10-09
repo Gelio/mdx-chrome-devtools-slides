e04d1353318cd8de7bb3ab577a1ef893464033d9
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-10-09 22:35:57 +0200

    add Video and PostponedVideo components

---
+++ components/postponed-video.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { updaters, withDeck } from 'mdx-deck';
+
+class PostponedVideo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+
+    const { update, index } = props.deck;
+    const { setSteps } = updaters;
+    update(setSteps(index, 1));
+  }
+
+  get _video() {
+    return this.videoRef.current;
+  }
+
+  componentDidMount() {
+    this._video.load();
+    this._updateVideoBasedOnStep();
+  }
+
+  componentDidUpdate() {
+    this._updateVideoBasedOnStep();
+  }
+
+  render() {
+    return <video ref={this.videoRef} {...this.props} />;
+  }
+
+  _updateVideoBasedOnStep() {
+    const { step } = this.props.deck;
+
+    if (step === 0) {
+      this._video.pause();
+    } else if (step === 1) {
+      this._video.play();
+    }
+  }
+}
+
+export default withDeck(PostponedVideo);
