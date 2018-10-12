f2f96e1d954d5e8448ca4b44d334f2c4c82bbc76
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-10-12 21:46:27 +0200

    add all slides
    Images are missing, some slides need rewording

--- slides/index.js
+++ slides/index.js
@@ -2,13 +2,16 @@ import Initial from './0-initial.mdx';
 import Motivation from './1-motivation.mdx';
 import BaseFunctionalities from './2-base-functionalities.mdx';
 import Debugging from './3-debugging.mdx';
-
-import TodoSlides from './todo-slides.mdx';
+import OtherFunctionalities from './4-other-functionalities.mdx';
+import Conclusion from './5-conclusion.mdx';
+import Final from './6-final.mdx';
 
 export default [
   ...Initial,
   ...Motivation,
   ...BaseFunctionalities,
   ...Debugging,
-  ...TodoSlides
+  ...OtherFunctionalities,
+  ...Conclusion,
+  ...Final
 ];
